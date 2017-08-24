5e576c939039c27c7e66b0c08a694cc0a3142c3b
Estudiante <Estudiante@10.203.2.105>
2017-08-24 14:54:35 -0500

    V1.0

---
+++ Taller/src/taller/LibraryCard.java
@@ -0,0 +1,18 @@
+package taller;
+
+public class LibraryCard {
+    private String owner;
+    public double borrowCnt;
+
+    public LibraryCard(String owner, double borrowCnt) {
+        this.owner = owner;
+        this.borrowCnt = borrowCnt;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
