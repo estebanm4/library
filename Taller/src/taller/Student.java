48becd0fc9b7ae37fb4378b70b178e19205bfb91
Estudiante <Estudiante@10.203.2.105>
2017-08-24 15:43:02 -0500

    V1.0

--- Taller/src/taller/Student.java
+++ Taller/src/taller/Student.java
@@ -3,8 +3,7 @@ package taller;
 public class Student {
     private String name;
     private String email;
-    
-    
+   
     public Student(String name, String email) {
         this.name = name;
         this.email = email;
