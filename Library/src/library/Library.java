746c7ab52454edada6fc5963d0edc40ab2b8f9c7
Estudiante <Estudiante@10.203.2.105>
2017-08-24 15:49:55 -0500

    V1.0

---
+++ Library/src/library/Library.java
@@ -0,0 +1,17 @@
+package library;
+/**
+  * 
+  * @author Estudiante Mateo Pacheco (cpachecof@unal.edu.co) Esteban Muñoz (jemunozva@unal.edu.co)
+ */
+public class Library {
+
+    public static void main(String[] args) {
+       
+        Student estudiante = new Student("Jon Java", "jj@javauniv.edu");
+        LibraryCard card1 = new LibraryCard(estudiante, 3);
+        LibraryCard card2 = new LibraryCard(estudiante, 0);
+        
+        System.out.println(card1);
+    }
+    
+}
---
+++ Library/src/library/LibraryCard.java
@@ -0,0 +1,32 @@
+package library;
+
+public class LibraryCard {
+    private Student owner;
+    public int borrowCnt;
+
+    public LibraryCard(Student owner, int borrowCnt) {
+        this.owner = owner;
+        this.borrowCnt = borrowCnt;
+    }
+
+    public Student getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Student owner) {
+        this.owner = owner;
+    }
+
+    public double getBorrowCnt() {
+        return borrowCnt;
+    }
+
+    public void setBorrowCnt(int borrowCnt) {
+        this.borrowCnt = borrowCnt;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryCard{" + "owner=" + owner + ", borrowCnt=" + borrowCnt + '}';
+    } 
+}
